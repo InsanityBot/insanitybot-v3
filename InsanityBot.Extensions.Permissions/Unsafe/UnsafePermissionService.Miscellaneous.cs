2742e4d66d96a889f725045613e8aaa66a8579fa
ExaInsanity <irisfeanora@outlook.com>
2022-06-05 22:07:26 +0200

    misc

--- InsanityBot.Extensions.Permissions/Unsafe/UnsafePermissionService.Miscellaneous.cs
+++ InsanityBot.Extensions.Permissions/Unsafe/UnsafePermissionService.Miscellaneous.cs
@@ -2,7 +2,10 @@ namespace InsanityBot.Extensions.Permissions.Unsafe;
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+
+using InsanityBot.Extensions.Permissions.Objects;
 
 using Starnight.Internal.Entities.Guilds;
 
@@ -12,36 +15,83 @@ public partial class UnsafePermissionService
 {
     public ValueTask SetParent(DiscordUser user, Int64 parent)
     {
-        throw new NotImplementedException();
+        UserPermissions userPermissions = this.__user_service.GetUserPermissions(user.Id)
+            ?? this.__user_service.CreateUserPermissions(user.Id);
+
+        userPermissions.AssignedRoles = Enumerable.Concat(userPermissions.AssignedRoles, Enumerable.Repeat(parent, 1))
+            .ToArray();
+
+        this.__user_service.WriteUserPermissions(userPermissions);
+
+        return ValueTask.CompletedTask;
     }
 
     public ValueTask SetParent(DiscordRole role, Int64 parent)
     {
-        throw new NotImplementedException();
+        RolePermissions rolePermissions = this.__role_service.GetRolePermissions(role.Id)
+            ?? this.__role_service.CreateRolePermissions(role.Id);
+
+        rolePermissions.Parents = Enumerable.Concat(rolePermissions.Parents, Enumerable.Repeat(parent, 1))
+            .ToArray();
+
+        this.__role_service.WriteRolePermissions(rolePermissions);
+
+        _ = Task.Run(this.rebuildRoleTrees);
+
+        return ValueTask.CompletedTask;
     }
 
-    public ValueTask SetParents(DiscordUser user, IEnumerable<Int64> parent)
+    public ValueTask SetParents(DiscordUser user, IEnumerable<Int64> parents)
     {
-        throw new NotImplementedException();
+        UserPermissions userPermissions = this.__user_service.GetUserPermissions(user.Id)
+            ?? this.__user_service.CreateUserPermissions(user.Id);
+
+        userPermissions.AssignedRoles = Enumerable.Concat(userPermissions.AssignedRoles, parents)
+            .ToArray();
+
+        this.__user_service.WriteUserPermissions(userPermissions);
+
+        return ValueTask.CompletedTask;
     }
 
-    public ValueTask SetParents(DiscordRole role, IEnumerable<Int64> parent)
+    public ValueTask SetParents(DiscordRole role, IEnumerable<Int64> parents)
     {
-        throw new NotImplementedException();
+        RolePermissions rolePermissions = this.__role_service.GetRolePermissions(role.Id)
+            ?? this.__role_service.CreateRolePermissions(role.Id);
+
+        rolePermissions.Parents = Enumerable.Concat(rolePermissions.Parents, parents)
+            .ToArray();
+
+        this.__role_service.WriteRolePermissions(rolePermissions);
+
+        _ = Task.Run(this.rebuildRoleTrees);
+
+        return ValueTask.CompletedTask;
     }
 
     public ValueTask RestoreDefaults(DiscordUser user)
     {
-        throw new NotImplementedException();
+        UserPermissions userPermissions = this.__user_service.CreateUserPermissions(user.Id);
+        this.__user_service.WriteUserPermissions(userPermissions);
+
+        return ValueTask.CompletedTask;
     }
 
     public ValueTask RestoreDefaults(DiscordRole role)
     {
-        throw new NotImplementedException();
+        RolePermissions rolePermissions = this.__role_service.CreateRolePermissions(role.Id);
+        this.__role_service.WriteRolePermissions(rolePermissions);
+
+        _ = Task.Run(this.rebuildRoleTrees);
+
+        return ValueTask.CompletedTask;
     }
 
     public ValueTask RestoreManifestDefaults()
     {
-        throw new NotImplementedException();
+        DefaultPermissions permissions = this.__default_service.CreateDefaultPermissions(this.__manifest);
+        this.__default_service.WriteDefaultPermissions(permissions);
+
+        return ValueTask.CompletedTask;
     }
 }
