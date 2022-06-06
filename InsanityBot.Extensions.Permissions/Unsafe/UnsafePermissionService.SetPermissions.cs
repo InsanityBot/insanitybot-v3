078e169bda89b24bc752d7b7c81f103f9015fbd1
ExaInsanity <irisfeanora@outlook.com>
2022-06-06 02:28:35 +0200

    set permissions

--- InsanityBot.Extensions.Permissions/Unsafe/UnsafePermissionService.SetPermissions.cs
+++ InsanityBot.Extensions.Permissions/Unsafe/UnsafePermissionService.SetPermissions.cs
@@ -4,79 +4,180 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-using Starnight.Internal.Entities.Guilds;
+using InsanityBot.Extensions.Permissions.Objects;
 
+using Starnight.Internal.Entities.Guilds;
 using Starnight.Internal.Entities.Users;
 
 public partial class UnsafePermissionService
 {
     public ValueTask GrantPermission(DiscordUser user, String permission)
     {
-        throw new NotImplementedException();
+        IEnumerable<String> permissions = this.resolveWildcards(permission);
+
+        UserPermissions userPermissions = this.__user_service.GetUserPermissions(user.Id)
+            ?? this.__user_service.CreateUserPermissions(user.Id);
+
+        foreach(String singlePermission in permissions)
+        {
+            userPermissions.Permissions[singlePermission] = PermissionValue.Allowed;
+        }
+
+        this.__user_service.WriteUserPermissions(userPermissions);
+
+        return ValueTask.CompletedTask;
     }
 
     public ValueTask GrantPermission(DiscordRole role, String permission)
     {
-        throw new NotImplementedException();
+        IEnumerable<String> permissions = this.resolveWildcards(permission);
+
+        RolePermissions rolePermissions = this.__role_service.GetRolePermissions(role.Id)
+            ?? this.__role_service.CreateRolePermissions(role.Id);
+
+        foreach(String singlePermission in permissions)
+        {
+            rolePermissions.Permissions[singlePermission] = PermissionValue.Allowed;
+        }
+
+        this.__role_service.WriteRolePermissions(rolePermissions);
+
+        return ValueTask.CompletedTask;
     }
 
-    public ValueTask GrantPermissions(DiscordUser user, IEnumerable<String> permissions)
+    public async ValueTask GrantPermissions(DiscordUser user, IEnumerable<String> permissions)
     {
-        throw new NotImplementedException();
+        foreach(String singlePermission in permissions)
+        {
+            await this.GrantPermission(user, singlePermission);
+        }
     }
 
-    public ValueTask GrantPermissions(DiscordRole role, IEnumerable<String> permissions)
+    public async ValueTask GrantPermissions(DiscordRole role, IEnumerable<String> permissions)
     {
-        throw new NotImplementedException();
+        foreach(String singlePermission in permissions)
+        {
+            await this.GrantPermission(role, singlePermission);
+        }
     }
 
     public ValueTask RevokePermission(DiscordUser user, String permission)
     {
-        throw new NotImplementedException();
+        IEnumerable<String> permissions = this.resolveWildcards(permission);
+
+        UserPermissions userPermissions = this.__user_service.GetUserPermissions(user.Id)
+            ?? this.__user_service.CreateUserPermissions(user.Id);
+
+        foreach(String singlePermission in permissions)
+        {
+            userPermissions.Permissions[singlePermission] = PermissionValue.Denied;
+        }
+
+        this.__user_service.WriteUserPermissions(userPermissions);
+
+        return ValueTask.CompletedTask;
     }
 
     public ValueTask RevokePermission(DiscordRole role, String permission)
     {
-        throw new NotImplementedException();
+        IEnumerable<String> permissions = this.resolveWildcards(permission);
+
+        RolePermissions rolePermissions = this.__role_service.GetRolePermissions(role.Id)
+            ?? this.__role_service.CreateRolePermissions(role.Id);
+
+        foreach(String singlePermission in permissions)
+        {
+            rolePermissions.Permissions[singlePermission] = PermissionValue.Denied;
+        }
+
+        this.__role_service.WriteRolePermissions(rolePermissions);
+
+        return ValueTask.CompletedTask;
     }
 
-    public ValueTask RevokePermissions(DiscordUser user, IEnumerable<String> permissions)
+    public async ValueTask RevokePermissions(DiscordUser user, IEnumerable<String> permissions)
     {
-        throw new NotImplementedException();
+        foreach(String singlePermission in permissions)
+        {
+            await this.RevokePermission(user, singlePermission);
+        }
     }
 
-    public ValueTask RevokePermissions(DiscordRole role, IEnumerable<String> permissions)
+    public async ValueTask RevokePermissions(DiscordRole role, IEnumerable<String> permissions)
     {
-        throw new NotImplementedException();
+        foreach(String singlePermission in permissions)
+        {
+            await this.RevokePermission(role, singlePermission);
+        }
     }
 
     public ValueTask UseFallback(DiscordUser user, String permission)
     {
-        throw new NotImplementedException();
+        IEnumerable<String> permissions = this.resolveWildcards(permission);
+
+        UserPermissions userPermissions = this.__user_service.GetUserPermissions(user.Id)
+            ?? this.__user_service.CreateUserPermissions(user.Id);
+
+        foreach(String singlePermission in permissions)
+        {
+            userPermissions.Permissions[singlePermission] = PermissionValue.Passthrough;
+        }
+
+        this.__user_service.WriteUserPermissions(userPermissions);
+
+        return ValueTask.CompletedTask;
     }
 
     public ValueTask UseFallback(DiscordRole role, String permission)
     {
-        throw new NotImplementedException();
+        IEnumerable<String> permissions = this.resolveWildcards(permission);
+
+        RolePermissions rolePermissions = this.__role_service.GetRolePermissions(role.Id)
+            ?? this.__role_service.CreateRolePermissions(role.Id);
+
+        foreach(String singlePermission in permissions)
+        {
+            rolePermissions.Permissions[singlePermission] = PermissionValue.Passthrough;
+        }
+
+        this.__role_service.WriteRolePermissions(rolePermissions);
+
+        return ValueTask.CompletedTask;
     }
 
-    public ValueTask UseFallbacks(DiscordUser user, IEnumerable<String> permissions)
+    public async ValueTask UseFallbacks(DiscordUser user, IEnumerable<String> permissions)
     {
-        throw new NotImplementedException();
+        foreach(String singlePermission in permissions)
+        {
+            await this.UseFallback(user, singlePermission);
+        }
     }
 
-    public ValueTask UseFallbacks(DiscordRole role, IEnumerable<String> permissions)
+    public async ValueTask UseFallbacks(DiscordRole role, IEnumerable<String> permissions)
     {
-        throw new NotImplementedException();
+        foreach(String singlePermission in permissions)
+        {
+            await this.UseFallback(role, singlePermission);
+        }
     }
 
     public ValueTask SetAdministrator(DiscordUser user, Boolean administrator)
     {
-        throw new NotImplementedException();
+        UserPermissions userPermissions = this.__user_service.GetUserPermissions(user.Id)
+            ?? this.__user_service.CreateUserPermissions(user.Id);
+
+        this.__user_service.WriteUserPermissions(userPermissions with { IsAdministrator = administrator });
+
+        return ValueTask.CompletedTask;
     }
 
     public ValueTask SetAdministrator(DiscordRole role, Boolean administrator)
     {
-        throw new NotImplementedException();
+        RolePermissions rolePermissions = this.__role_service.GetRolePermissions(role.Id)
+            ?? this.__role_service.CreateRolePermissions(role.Id);
+
+        this.__role_service.WriteRolePermissions(rolePermissions with { IsAdministrator = administrator });
+
+        return ValueTask.CompletedTask;
     }
 }
